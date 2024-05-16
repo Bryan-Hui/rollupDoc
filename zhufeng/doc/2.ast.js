4682b6e0bfade16067f51fc42fa5f9805cec79de
BryanHui <928102064@qq.com>
2024-05-16 20:00:00 +0800

    del: 删除无效代码

--- zhufeng/doc/2.ast.js
+++ zhufeng/doc/2.ast.js
@@ -1,2 +1,15 @@
-import acorn from 'acorn'
+const acorn = require('acorn')
+
+let astTree = acorn.parse("import $ from 'jquery'");    
+console.log('astTree ',astTree)
+
+
+let  a = 3
+const b = 5
+
+
+const fn  =   () => {}
+
+
+
 
